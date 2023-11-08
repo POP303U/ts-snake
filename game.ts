cce9ce63ee9a0e56f81a6ccf474af3d1c720dec5
POP303U <nicoanton.gering@gmail.com>
2023-11-08 14:28:27 +0100

    fixed compile error

--- game.ts
+++ game.ts
@@ -5,70 +5,70 @@ const gridWidth = canvas.width / gridSize;
 const gridHeight = canvas.height / gridSize;
 
 interface Segment {
-    x: number;
-    y: number;
+x: number;
+y: number;
 }
 
 const food: Segment = { x: 15, y: 10 };
 let snake: Segment[] = [{ x: 10, y: 10 }];
-let direction: Segment = { x: 1, y: 0 };
+let direction: { x: number; y: number } = { x: 1, y: 0 }; // Explicitly define direction type}}
 
 function drawSnake() {
-    ctx.fillStyle = "black";
-    snake.forEach(segment => {
-        ctx.fillRect(segment.x * gridSize, segment.y * gridSize, gridSize, gridSize);
-    });
+ctx.fillStyle = "black";
+snake.forEach(segment => {
+    ctx.fillRect(segment.x * gridSize, segment.y * gridSize, gridSize, gridSize);
+});
 }
 
 function drawFood() {
-    ctx.fillStyle = "red";
-    ctx.fillRect(food.x * gridSize, food.y * gridSize, gridSize, gridSize);
+ctx.fillStyle = "red";
+ctx.fillRect(food.x * gridSize, food.y * gridSize, gridSize, gridSize);
 }
 
 function moveSnake() {
-    const newHead: Segment = {
-        x: snake[0].x + direction.x,
-        y: snake[0].y + direction.y,
-    };
-    snake.unshift(newHead);
+const newHead: Segment = {
+    x: snake[0].x + direction.x,
+    y: snake[0].y + direction.y,
+};
+snake.unshift(newHead);
 
-    if (newHead.x === food.x && newHead.y === food.y) {
-        generateFood();
-    } else {
-        snake.pop();
-    }
+if (newHead.x === food.x && newHead.y === food.y) {
+    generateFood();
+} else {
+    snake.pop();
+}
 }
 
 function generateFood() {
-    food.x = Math.floor(Math.random() * gridWidth);
-    food.y = Math.floor(Math.random() * gridHeight);
+food.x = Math.floor(Math.random() * gridWidth);
+food.y = Math.floor(Math.random() * gridHeight);
 }
 
 function checkCollision() {
-    const head = snake[0];
-    return (
-        head.x < 0 ||
-        head.y < 0 ||
-        head.x >= gridWidth ||
-        head.y >= gridHeight ||
-        snake.some(segment => segment !== head && segment.x === head.x && segment.y === head.y)
-    );
+const head = snake[0];
+return (
+    head.x < 0 ||
+    head.y < 0 ||
+    head.x >= gridWidth ||
+    head.y >= gridHeight ||
+    snake.some(segment => segment !== head && segment.x === head.x && segment.y === head.y)
+);
 }
 
 function gameLoop() {
-    if (checkCollision()) {
-        alert("Game Over!");
-        snake = [{ x: 10, y: 10 }];
-        direction = { x: 1, y: 0 };
-    }
+if (checkCollision()) {
+    alert("Game Over!");
+    snake = [{ x: 10, y: 10 }];
+    direction = { x: 1, y: 0 };
+}
 
-    ctx.clearRect(0, 0, canvas.width, canvas.height);
+ctx.clearRect(0, 0, canvas.width, canvas.height);
 
-    drawSnake();
-    drawFood();
-    moveSnake();
+drawSnake();
+drawFood();
+moveSnake();
 
-    setTimeout(gameLoop, 100);
+setTimeout(gameLoop, 100);
 }
 
 document.addEventListener("keydown", event => {
@@ -77,13 +77,13 @@ document.addEventListener("keydown", event => {
         case 37: // Left arrow
             direction = { x: -1, y: 0 };
             break;
-        case 38: // Up arrow
+        case 38:    // Up arrow
             direction = { x: 0, y: -1 };
             break;
-        case 39: // Right arrow
+        case 39:    // Right arrow
             direction = { x: 1, y: 0 };
             break;
-        case 40: // Down arrow
+        case 40:    // Down arrow
             direction = { x: 0, y: 1 };
             break;
     }
